0756cea49ff3e0945d67f19f313b6e79ba4faa87
mhmansoo <mhmansoo@andrew.cmu.edu>
2025-08-31 16:32:10 +0300

    Fix typing in fib and fibRoute to remove any usage

--- src/fib.ts
+++ src/fib.ts
@@ -1,14 +1,7 @@
 // src/fib.ts
-function fibonacci(n: number): number {
-  if (n < 0) {
-    return -1;
-  } else if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
-  }
-
+export default function fibonacci(n: number): number {
+  if (n < 0) return -1;
+  if (n === 0) return 0;
+  if (n === 1) return 1;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
-
-export default fibonacci;
